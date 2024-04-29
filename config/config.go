2b53e85546cdc278cd8d806de37604f9ca9009a2
ribbinpo <ribbinpo@gmail.com>
2024-04-29 19:41:28 +0700

    feat: :sparkles: add config env

--- config/config.go
+++ config/config.go
@@ -14,6 +14,13 @@ type Config struct {
 	Db  *Db
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func NewConfig(pathEnv string) *Config {
 	err := godotenv.Load(pathEnv)
 	if err != nil {
@@ -24,18 +31,28 @@ func NewConfig(pathEnv string) *Config {
 		log.Fatal("Error loading timezone")
 	}
 
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_PORT"),
+	// 	os.Getenv("DB_SSLMODE"))
+	// url := fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"))
-	url := fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+		getEnv("DB_HOST", os.Getenv("DB_HOST")),
+		getEnv("DB_USER", os.Getenv("DB_USER")),
+		getEnv("DB_PASSWORD", os.Getenv("DB_PASSWORD")),
+		getEnv("DB_NAME", os.Getenv("DB_NAME")),
+		getEnv("DB_PORT", os.Getenv("DB_PORT")),
+		getEnv("DB_SSLMODE", os.Getenv("DB_SSLMODE")))
+	url := fmt.Sprintf("%s:%s", getEnv("DB_HOST", os.Getenv("DB_HOST")), getEnv("DB_PORT", os.Getenv("DB_PORT")))
 
 	return &Config{
 		App: &App{
-			Port:     ":" + os.Getenv("PORT"),
+			// Port:     ":" + os.Getenv("PORT"),
+			Port:     ":" + getEnv("PORT", os.Getenv("PORT")),
 			TimeZone: tz,
 		},
 		Db: &Db{
